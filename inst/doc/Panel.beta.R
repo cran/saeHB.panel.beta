b568d60f6b8cb706b042025f4ea5393ea9940a76
Dian Rahmawati Salis <dianrahmawatisalis03@gmail.com>
2023-06-20 04:00:02 +0000

    version 0.1.2

--- inst/doc/Panel.beta.R
+++ inst/doc/Panel.beta.R
@@ -9,10 +9,10 @@ library(saeHB.panel.beta)
 data("dataPanelbeta")
 
 ## -----------------------------------------------------------------------------
-dataPanelbeta = dataPanelbeta[1:25,] #for the example only use part of the dataset
-area = max(dataPanelbeta[,2])
-period = max(dataPanelbeta[,3])
-result=Panel.beta(ydi~xdi1+xdi2,area=area, period=period ,iter.mcmc = 10000,thin=5,burn.in = 1000,data=dataPanelbeta)
+dataPanelbeta <- dataPanelbeta[1:25,] #for the example only use part of the dataset
+area <- max(dataPanelbeta[,2])
+period <- max(dataPanelbeta[,3])
+result<-Panel.beta(ydi~xdi1+xdi2,area=area, period=period ,iter.mcmc = 10000,thin=5,burn.in = 1000,data=dataPanelbeta)
 
 ## -----------------------------------------------------------------------------
 result$Est
@@ -24,22 +24,22 @@ result$coefficient
 result$refvar
 
 ## -----------------------------------------------------------------------------
-MSE_HB=result$Est$SD^2
+MSE_HB<-result$Est$SD^2
 summary(MSE_HB)
 
 ## -----------------------------------------------------------------------------
-RSE_HB=sqrt(MSE_HB)/result$Est$MEAN*100
+RSE_HB<-sqrt(MSE_HB)/result$Est$MEAN*100
 summary(RSE_HB)
 
 ## -----------------------------------------------------------------------------
-y_dir=dataPanelbeta[,1]
-y_HB=result$Est$MEAN
-y=as.data.frame(cbind(y_dir,y_HB))
+y_dir<-dataPanelbeta[,1]
+y_HB<-result$Est$MEAN
+y<-as.data.frame(cbind(y_dir,y_HB))
 summary(y)
-MSE_dir=dataPanelbeta[,4]
-MSE=as.data.frame(cbind(MSE_dir, MSE_HB))
+MSE_dir<-dataPanelbeta[,4]
+MSE<-as.data.frame(cbind(MSE_dir, MSE_HB))
 summary(MSE)
-RSE_dir=sqrt(MSE_dir)/y_dir*100
-RSE=as.data.frame(cbind(RSE_dir, RSE_HB))
+RSE_dir<-sqrt(MSE_dir)/y_dir*100
+RSE<-as.data.frame(cbind(RSE_dir, RSE_HB))
 summary(RSE)
 
